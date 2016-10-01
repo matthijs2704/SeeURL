d3cefb76431fc5661ce1364efaee22ce433462a3
Matthijs Logemann <matthijs.logemann@xs4all.nl>
2016-10-01 13:20:06 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@ import PackageDescription
 
 let package = Package(
     name: "SeeURL",
-    dependencies: [
-        .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 2, minor: 2),
-        .Package(url: "https://github.com/PureSwift/CcURL.git", majorVersion: 1, minor: 1)
-    ],
     targets: [
         Target(
             name: "UnitTests",
             dependencies: [.Target(name: "SeeURL")]),
         Target(
             name: "SeeURL")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/PureSwift/SwiftFoundation.git", majorVersion: 2, minor: 2),
+        .Package(url: "https://github.com/PureSwift/CcURL.git", majorVersion: 1, minor: 1)
     ]
 )
